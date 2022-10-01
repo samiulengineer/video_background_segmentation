567a9b6e5e53b3293a963a0de26255d9df122158
MD Samiul Islam <samiulengineer@gmail.com>
2022-10-01 01:52:21 -0600

    init

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
-# video_background_segmentation
+# video_background_subtraction
+
+## Experiments
+
+After setup the required folders and package run one of the following experiment. There are four experiments based on combination of parameters passing through `argparse` and `config.yaml`. Combination of each experiments given below.
+
+When you run the following code based on different experiments, some new directories will be created;
+
+1. csv_logger (save all evaluation result in csv format)
+2. logs (tensorboard logger)
+3. model (save model checkpoint)
+4. prediction (validation and test prediction png format)
+
+* **Comprehensive Full Resolution with Class Balance (CFR-CB)**: We balance the dataset biasness towards non-water class in this experiment.
+
+```
+python train.py --root_dir YOUR_ROOT_DIR \
+    --dataset_dir YOUR_ROOT_DIR/data/ \
+    --model_name unet \
+    --epochs 10 \
+    --batch_size 10 \
+    --index -1 \
+    --experiment cfr_cb \
+    --patchify False \
+    --height 240 \
+    --width 320 \
+    --weights True \
+```
+
+* **Patchify Half Resolution with Class Balance (PHR-CB)**: In this experiment we take a threshold value (19%) of water class and remove the patch images for each chip that are less than threshold value.
+
+```
+python train.py --root_dir YOUR_ROOT_DIR \
+    --dataset_dir YOUR_ROOT_DIR/data/ \
+    --model_name unet \
+    --epochs 10 \
+    --batch_size 3 \
+    --index -1 \
+    --experiment phr_cb \
+    --patchify True \
+    --patch_size 256 \
+    --weights False \
+    --patch_class_balance True
+```
+
+## Testing
+
+* **CFR-CB Experiment**
+
+Run following model for evaluating train model on test dataset.
+
+```
+python test.py \
+    --dataset_dir YOUR_ROOT_DIR/data/ \
+    --model_name unet \
+    --load_model_name MODEL_CHECKPOINT_NAME \
+    --plot_single False \
+    --index -1 \
+    --patchify False \
+    --height 240 \
+    --width 320 \
+    --experiment cfr_cb \
+    --weights True \
+```
+
+* **PHR-CB Experiment**
+
+```
+python test.py \
+    --dataset_dir YOUR_ROOT_DIR/data/ \
+    --model_name unet \
+    --load_model_name my_model.hdf5 \
+    --plot_single False \
+    --index -1 \
+    --patchify True \
+    --experiment phr_cb \
+    --weights True \
+```
---
+++ background_subtraction_cv/backround_subtraction_cv.py
@@ -0,0 +1,96 @@
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+def capBackgroud(directory):
+
+    imgList = []
+
+    for i in os.listdir(directory):
+        if i.endswith(".jpg"):
+            img = cv2.imread(directory+"/"+i)
+            imgList.append(img)
+
+    background = np.mean(imgList, axis=0).astype(dtype=np.uint8)
+    return background
+
+
+def saveImg(imgs, imgName):
+
+    fig, axs = plt.subplots(2, 3, figsize=(15, 8))
+    titles = ['Orginal Image', 'Background Image', 'Background Subtraction',
+              'Binary masking', 'Opening(2 times)', 'Opening(4 times)']
+    axs = axs.flatten()
+    for img, ax, ti in zip(imgs, axs, titles):
+        ax.imshow(img, cmap='gray')
+        ax.set_title(ti)
+
+    fig.tight_layout()
+    plt.savefig(imgName, bbox_inches='tight', dpi=800)
+    plt.clf()   # clears an axis
+    plt.cla()   # clears the entire current figure with all its axes, but leaves the window opened
+    plt.close()  # closes a window
+
+
+def subBackground(img, background, threshold=30):
+
+    subBack = cv2.absdiff(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY),
+                          cv2.cvtColor(background, cv2.COLOR_BGR2GRAY))
+    ret, bw_img = cv2.threshold(subBack, threshold, 255, cv2.THRESH_BINARY)
+
+    return subBack, bw_img
+
+
+def opening(img):
+
+    kernelSizes = [(1, 1), (2, 2)]
+    openImg = img
+    # loop over the kernels sizes
+    for kernelSize in kernelSizes:
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, kernelSize)
+        openImg = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+
+    return openImg
+
+
+def gaussian(img):
+
+    image = cv2.GaussianBlur(img, (5, 5), 1)
+    #gaussianImg = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 3, 2)
+
+    return image
+
+
+if __name__ == "__main__":
+
+    directory = "/home/mdsamiul/github_project/video_background_subtraction/data/highway/input"
+
+    # Capturing Background Image using median method
+    background = capBackgroud(directory)
+
+    # creating output directory
+    outDir = "/home/mdsamiul/github_project/video_background_subtraction/background_subtraction_cv/output"
+    if not os.path.exists(outDir):
+        os.makedirs(outDir)
+
+    for i in os.listdir(directory):
+        if i.endswith(".jpg"):
+            img = cv2.imread(directory+"/"+i)
+
+            # Subtracting Background and get binary mask
+            subBack, bw_img = subBackground(img, background)
+
+            # Applying opening technique (erosion followed by dilation)
+            openingImg = opening(bw_img)
+            openingImg2 = opening(openingImg)
+            # Gaussian
+            # gaussianImg = gaussian(openingImg)
+
+            imgName = outDir + "/" + i  # figure name
+            # save figure in output directory
+            saveImg([img, background, subBack, bw_img,
+                    openingImg, openingImg2], imgName)
+            
